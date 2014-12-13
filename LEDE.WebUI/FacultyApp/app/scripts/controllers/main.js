38bcf70d6df7d5878ba9b35032f1b047412261c6
John Horne <jhhorne@gmail.com>
2014-12-12 20:00:17 -0800

    added angular app to project

---
+++ LEDE.WebUI/FacultyApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name facultyAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the facultyAppApp
+ */
+angular.module('facultyAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
